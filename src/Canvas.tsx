dfa9f1ede746f999d4f10ca4f87486e5ee978403
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.com.br>
2025-11-14 18:28:21 -0300

    draw first line

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -11,12 +11,12 @@ function Canvas({ width, height }: { width: number; height: number }) {
 		if (!ctx) return; // Ensure context exists
 
 		// Your drawing logic here
-		ctx.fillStyle = "blue";
-		ctx.fillRect(0, 0, width - 100, height - 100);
-
-		ctx.fillStyle = "red";
-		ctx.arc(width / 2, height / 2, 50, 0, Math.PI * 2);
-		ctx.fill();
+		ctx.beginPath(); // reset ctx state
+		ctx.strokeStyle = "red"; // define color of the line
+		ctx.lineWidth = 10; // thickness of the line
+		ctx.moveTo(30, 70); // moveTo(x,y) -> starting point of the line
+		ctx.lineTo(130, 70); // line(x,y) -> end point of the line
+		ctx.stroke(); // draws the line
 	}, [width, height]); // Redraw if width or height changes
 
 	return (
